99d3d36d000776812b342ed7b78c9033310ec34d
Vladimir Golodaev <golodaevvladimir@gmail.com>
2017-12-11 23:27:23 +0300

    ex.3 need to fix

--- browser/gallery/slider.js
+++ browser/gallery/slider.js
@@ -1,25 +1,25 @@
-var slider = document.getElementById('currentPhoto');
-const images = [];
+const slider = document.getElementById('currentPhoto');
+const images = [
+	'i/breuer-building.jpg',
+	'i/guggenheim-museum.jpg',
+	'i/headquarters.jpg',
+	'i/IAC.jpg',
+	'i/new-museum.jpg'
+];
 let img = 0;
 
-images.push('i/breuer-building.jpg', 'i/guggenheim-museum.jpg', 'i/headquarters.jpg', 'i/IAC.jpg', 'i/new-museum.jpg');
-
-setInterval(() => {
-	slider.src = images[img];
-	img === 4 ? img = 0 : img += 1;
-}, 5000);
-
-var nextImg = document.getElementById('nextPhoto');
-var prevImg = document.getElementById('prevPhoto');
+const nextImg = document.getElementById('nextPhoto');
+const prevImg = document.getElementById('prevPhoto');
 
 nextImg.onclick = function () {
 	slider.src = images[img];
-	img === 4 ? img = 0 : img += 1;
-
+	if (img < images.length) img += 1;
+	if (img === images.length) img = 0;
 };
 
 prevImg.onclick = function () {
 	slider.src = images[img];
-	img === 0 ? img = 4 : img -= 1;
+	if (img < images.length) img -= 1;
+	// if (img === 0) img = images.length - 1;
 
 };
