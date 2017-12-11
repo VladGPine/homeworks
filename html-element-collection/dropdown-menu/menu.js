ea3b4a23da8b3350f4c22cc610bb77e0c4ddf491
Vladimir Golodaev <golodaevvladimir@gmail.com>
2017-12-12 00:01:01 +0300

    ex.1 done

--- html-element-collection/dropdown-menu/menu.js
+++ html-element-collection/dropdown-menu/menu.js
@@ -1,6 +1,4 @@
 const dropdown = document.getElementsByClassName('wrapper-dropdown')[0];
 dropdown.onclick = function () {
-		if (dropdown.classList.contains('wrapper-dropdown')) {
-			return dropdown.classList.toggle('active');
-		}
+		if (dropdown.classList.contains('wrapper-dropdown')) return dropdown.classList.toggle('active');
 }
