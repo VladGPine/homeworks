ffda3057fc014c6a407f1472ca56c87ef286268f
Vladimir Golodaev <golodaevvladimir@gmail.com>
2017-12-12 23:18:06 +0300

    ex.2 has been added

--- dom/shop/js/price-format.js
+++ dom/shop/js/price-format.js
@@ -1,3 +1,18 @@
 function getPriceFormatted(value) {
   return  value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 }
+
+const cartCount = document.querySelector('#cart-count');
+const totalPrice = document.querySelector('#cart-total-price');
+
+function addItem() {
+	let currCount = +cartCount.innerHTML + 1;
+	cartCount.innerHTML = currCount;
+	let currPrice = totalPrice.dataset.price ? +totalPrice.dataset.price : 0;
+	currPrice += +this.dataset.price;
+	totalPrice.dataset.price = currPrice;
+	totalPrice.innerHTML = getPriceFormatted(currPrice);
+}
+
+let items = Array.from(document.querySelectorAll('button.add'));
+items.forEach(item => item.addEventListener('click', addItem));
