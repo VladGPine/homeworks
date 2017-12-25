894061bfee8720129d9fdd6f3815f71ea7c71f77
Vladimir Golodaev <golodaevvladimir@gmail.com>
2017-12-25 23:47:15 +0300

    ex.2 done

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,11 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+	let dataPropName = event.target;
+	event.stopPropagation();
+	let propName = dataPropName.dataset.propName;
+	let dir = dataPropName.dataset.dir ? -dataPropName.dataset.dir : 1;
+	dataPropName.dataset.dir = dir;
+	this.dataset.sortBy = propName;
+	sortTable(propName, dir);
 }
