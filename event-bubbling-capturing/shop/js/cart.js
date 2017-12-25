2130c8ff9f78ff8a52e41bdb313c82792e249dc0
Vladimir Golodaev <golodaevvladimir@gmail.com>
2017-12-25 23:50:35 +0300

    ex.3 done

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,10 @@
 'use strict';
+document.querySelector('.items-list').addEventListener('click', function (event) {
+	event.preventDefault();
+	if (event.target.classList.contains('add-to-cart')) {
+		addToCart({
+			title: event.target.getAttribute('data-title'),
+			price: event.target.getAttribute('data-price')
+		});
+	}
+});
