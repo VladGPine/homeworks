9702ece65b33df2632bbae4fb2a9702d4f21493b
Vladimir Golodaev <golodaevvladimir@gmail.com>
2017-12-28 17:08:30 +0300

    ex.1 done

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,31 @@
 'use strict';
+const counter = document.getElementById('counter');
+const buttons = document.querySelectorAll('.wrap-btns button');
+let count;
+
+document.addEventListener('DOMContentLoaded', function() {
+	let localCount = localStorage.getItem('count');
+	if (localCount !== null) {
+		count = parseInt(localCount);
+	} else {
+		count = 0;
+		counter.textContent = count;
+	}
+
+});
+
+function changeCount(event) {
+	if (event.target.id == 'increment') {
+		count += 1;
+	} else if (event.target.id == 'decrement' && count > 0) {
+		count -= 1;
+	} else {
+		count = 0;
+	}
+	counter.textContent = count;
+	localStorage.setItem('count', count);
+}
+
+for (const button of buttons) {
+	button.addEventListener('click', changeCount);
+}
