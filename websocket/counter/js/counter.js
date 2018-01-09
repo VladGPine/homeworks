2aa371924dfb27c833c818f589607b9b7e3edf08
Vladimir Golodaev <golodaevvladimir@gmail.com>
2018-01-09 23:56:43 +0300

    ex.2 done

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,18 @@
 'use strict';
+const counter = document.querySelector('.counter');
+const outputErrors = document.querySelector('output.errors');
+const connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+connection.addEventListener('message', (event) => {
+	let message = JSON.parse(event.data);
+	counter.textContent = message.connections;
+	outputErrors.value = message.errors;
+});
+
+connection.addEventListener('error', (error) => {
+	console.log('Произошла ошибка: ${error.data}');
+});
+
+window.addEventListener('beforeunload', () => {
+	connection.close(1000);
+});
