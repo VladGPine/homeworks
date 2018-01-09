2c81c597609365498b3187f4ed556d4141b813f1
Vladimir Golodaev <golodaevvladimir@gmail.com>
2018-01-09 23:52:21 +0300

    ex.1 done

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,19 @@
-'use strinct';
+'use strict';
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+connection.addEventListener('open', (event) => {
+	showBubbles(event.currentTarget);
+});
+
+document.addEventListener('click', (event) =>{
+	let coordinates = { x: event.clientX, y: event.clientY};
+	connection.send(JSON.stringify(coordinates));
+});
+
+connection.addEventListener('error', error => {
+	console.log('Произошла ошибка: ${error.data}');
+});
+
+window.addEventListener('beforeunload', () => {
+	connection.close(1000);
+});
