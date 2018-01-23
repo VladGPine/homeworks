e50f5512128e98d6d3fe945e5673024362e09788
Vladimir Golodaev <golodaevvladimir@gmail.com>
2018-01-23 23:26:56 +0300

    ex.1 done

--- dom-from-zero/comments/js/comments.js
+++ dom-from-zero/comments/js/comments.js
@@ -1,31 +1,56 @@
 'use strict';
 
 function showComments(list) {
-  const commentsContainer = document.querySelector('.comments');
-  const comments = list.map(createComment).join('');
-  commentsContainer.innerHTML += comments;
+	const commentsContainer = document.querySelector('.comments');
+	const comments = list.forEach((data) => {
+		commentsContainer.appendChild(createComment(data));
+	});
+}
+function createEl(type, className, title, style) {
+	let element = document.createElement(type);
+	element.className = className;
+	if (title) {
+		element.title = title;
+	}
+	if (style) {
+		element.style.cssText = style;
+	}
+	return element;
 }
 
 function createComment(comment) {
-  return `<div class="comment-wrap">
-    <div class="photo" title="${comment.author.name}">
-      <div class="avatar" style="background-image: url('${comment.author.pic}')"></div>
-    </div>
-    <div class="comment-block">
-      <p class="comment-text">
-        ${comment.text.split('\n').join('<br>')}
-      </p>
-      <div class="bottom-comment">
-        <div class="comment-date">${new Date(comment.date).toLocaleString('ru-Ru')}</div>
-        <ul class="comment-actions">
-          <li class="complain">Пожаловаться</li>
-          <li class="reply">Ответить</li>
-        </ul>
-      </div>
-    </div>
-  </div>`
+	let wrapper = createEl('div', 'comment-wrap')
+	let photoDiv = createEl('div', 'photo', comment.author.name);
+	let divAva = createEl('div', 'avatar', false, 'background-image: url(' + comment.author.pic + ')');
+	photoDiv.appendChild(divAva);
+	wrapper.appendChild(photoDiv);
+	let commentBlock = createEl('div', 'comment-block');
+	let commentText = createEl('p', 'comment-text')
+
+	comment.text.split('\n').map((data) => {
+		let tn = document.createTextNode(data);
+		commentText.appendChild(tn);
+		commentText.appendChild(document.createElement('br'));
+	});
+	commentBlock.appendChild(commentText);
+	let bottomComment = createEl('div', 'bottom-comment');
+	let commentDate = createEl('div', 'comment-date');
+	commentDate.innerText = new Date(comment.date).toLocaleString('ru-Ru');
+	bottomComment.appendChild(commentDate);
+	let commentActions = createEl('ul', 'comment-actions');
+	let liComplain = createEl('li', 'complain');
+	liComplain.textContent = 'Пожаловаться';
+	let liReply = createEl('li', 'reply');
+	liReply.textContent = 'Ответить';
+	commentActions.appendChild(liComplain);
+	commentActions.appendChild(liReply);
+	bottomComment.appendChild(commentActions);
+	commentBlock.appendChild(bottomComment);
+	wrapper.appendChild(commentBlock);
+	return wrapper;
 }
 
+
 fetch('https://neto-api.herokuapp.com/comments')
-  .then(res => res.json())
-  .then(showComments);
+	.then(res => res.json())
+	.then(showComments);
