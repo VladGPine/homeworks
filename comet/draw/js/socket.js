d905b443b2fd336606071ecdef485eaffe27fbee
Vladimir Golodaev <golodaevvladimir@gmail.com>
2018-01-25 00:13:49 +0300

    ex.2 done

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const socket = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+socket.addEventListener('open', (event) => {
+	console.log('Соединение открыто');
+	console.log(window.editor);
+});
+
+socket.addEventListener('message', (event) => {
+	console.log(event.data);
+});
+
+socket.addEventListener('close', (event) => {
+	console.log('Соединение закрыто');
+	console.log(event.code);
+});
+
+socket.addEventListener('error', (event) => {
+	console.log(event.data);
+})
+window.editor.addEventListener('update', (event) => {
+	console.log(event);
+	let canvas = event.canvas;
+	canvas.toBlob((blob) => {
+		socket.send(blob);
+	})
+});
